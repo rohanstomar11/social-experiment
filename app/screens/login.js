572d9693bb871851da164b9d998533791076fb05
HimanshuJoshi228 <himanshu.joshi@thecareerlabs.com>
2022-05-06 19:08:33 +0530

    Login

--- app/screens/login.js
+++ app/screens/login.js
@@ -1,21 +1,43 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import CustomInputField from '../components/CustomInputField'
+import CustomButton from '../components/CustomButton'
+import { COLORS } from '../assets/color'
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
   const [email, setEmail] = useState('')
   return (
     <View style={{
       flex: 1,
-      justifyContent: 'center',
+      justifyContent: 'space-around',
       alignItems: 'center',
+      backgroundColor: COLORS.black,
     }}>
-      <CustomInputField placeholder={"Email"}/>
-      <CustomInputField placeholder={"Password"} top={20} hide={true}/>
-      {/* <TouchableOpacity 
+      <View>
+        <Text
+          style={{ fontSize: 25, fontWeight: 'bold', color: COLORS.black,}}
+        >
+          LoginScreen
+        </Text>
+      </View>
+      <View style={{width: '100%', alignItems: 'center'}}>
+        <CustomInputField placeholder={"Email"} />
+        <CustomInputField placeholder={"Password"} top={20} hide={true} />
+        {/* <TouchableOpacity 
         onPress={()=>{navigation.navigate('SignupScreen')}}>
         <Text>LoginScreen</Text>
       </TouchableOpacity> */}
+        <CustomButton
+          title={'LogIn'}
+          onPress={() => { navigation.navigate('SignupScreen') }}
+          style={{ width: '85%', marginTop: 20, }}
+          fontsize={20}
+        />
+      </View>
+      <View style={{ flexDirection: 'row' }}>
+        <Text>Are you new here?</Text>
+        <Text>SignUp</Text>
+      </View>
     </View>
   )
 }
