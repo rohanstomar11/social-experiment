54b560ade2d464f3214c8ceafb84f51fbbfaf0a2
RohanSingh <rohansingh.tomar@thecareerlabs.com>
2022-05-11 15:39:32 +0530

    added get social sdk

--- android/app/src/main/java/com/social_experiment/MainApplication.java
+++ android/app/src/main/java/com/social_experiment/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.soloader.SoLoader;
 import com.social_experiment.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import im.getsocial.rn.RNGetSocialPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
